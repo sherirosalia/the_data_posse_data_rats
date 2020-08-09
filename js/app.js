0797a1593ad5bb3f35564d58738a1e48817668c6
Sheri Rosalia <sheri.rosalia@gmail.com>
2020-08-09 11:04:13 -0700

    styling

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@
 // });	
 
 
-var mouseyImages = ['images/m_da.svg', 'images/m_legs.svg', 'images/m_sleep.png', 'images/m_up.svg', 'images/m_sv.svg', 'images/m_ls.svg' ];
+var mouseyImages = ['images/m_da.svg', 'images/m_legs.svg', 'images/m_up.svg', 'images/m_sv.svg', 'images/m_ls.svg' ];
 let mouseyDiv = document.getElementById('mouse1');
 var arrayLength = mouseyImages.length;
 for (let i=1; i<arrayLength; i++) {
   
     setTimeout( function timer(){
       mouseyDiv.innerHTML += '<img src=\"'+mouseyImages[i]+'\"  >';
-    }, i*1500 );
+    }, i*1200 );
 
 }
