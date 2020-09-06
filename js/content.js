6fb8c85396933adc255083b99c3dad64ad6e93b0
Sheri Rosalia <sheri.rosalia@gmail.com>
2020-09-06 13:33:38 -0700

    commit

--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
 function ratRates(){
     let containerDiv=document.getElementById("ratRates"),
-    url="https://public.tableau.com/views/DATA_RATS/BadHabitsofHollywood?:language=en&:display_count=y&:origin=viz_share_link";
-
+    // url="https://public.tableau.com/views/DATA_RATS/BadHabitsofHollywood?:language=en&:display_count=y&:origin=viz_share_link";
+    url='https://public.tableau.com/views/DATA_RATS/BadHabitsofHollywood?:language=en&:display_count=y&:origin=viz_share_link'
     let viz = new tableau.Viz(containerDiv, url);
     
 }
